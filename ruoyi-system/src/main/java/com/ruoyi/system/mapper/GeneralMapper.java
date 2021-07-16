0dfb4f11180e27d2f4536b0ac31d204b0e6dc6cf
武继跃 <747506908@qq.com>
2021-07-16 17:03:30 +0800

    全新版本v4.4.0

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/GeneralMapper.java
@@ -0,0 +1,15 @@
+package com.ruoyi.system.mapper;
+
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+public interface GeneralMapper {
+
+    void generalInsert(@Param("tableName") String tableName,@Param("data") Map<String, Object> data);
+    List<String> selectPrimaryKeys(@Param("tableName") String tableName);
+    List<String> selectAllColumns(@Param("tableName") String tableName);
+    List<String> selectAllTables();
+    List<String> selectColumnExtraInfo(@Param("tableName") String tableName,@Param("columnName") String columnName);
+}
